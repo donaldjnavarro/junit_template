da7b310c8bbaa8e60b3491f14150c77f892172b5
Donny Navarro <donaldjnavarro@gmail.com>
2025-03-12 13:51:06 -0400

    Expanded example tests

--- src/main/java/com/symflower/demo/App.java
+++ src/main/java/com/symflower/demo/App.java
@@ -4,7 +4,7 @@ package com.symflower.demo;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static String helloWorld(String[] args) {
+        return "Hello World: " + args[0];
     }
 }
